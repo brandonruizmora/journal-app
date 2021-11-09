3b56ea71daf0d2cc847a2f20c3f0a96a60b91986
brandonruizmora <brandon_ruizmor@outlook.es>
2021-11-08 22:19:45 -0600

    added routes and components

--- src/JournalApp.js
+++ src/JournalApp.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { AppRouter } from './routes/AppRouter';
 
 export const JournalApp = () => {
     return (
         <div>
-            JournalApp
+            <AppRouter />
         </div>
     )
 }
---
+++ src/routes/AuthRouter.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import { LoginScreen } from '../components/auth/LoginScreen';
+import { RegisterScreen } from '../components/auth/RegisterScreen';
+
+export const AuthRouter = () => {
+    return (
+        <>
+            <Routes>
+                <Route path="login" element={<LoginScreen />} />
+                <Route path="register" element={<RegisterScreen />} />
+            </Routes>
+        </>
+    )
+}
