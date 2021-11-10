9bc6b46f54f6124bc3e0200d539049080d4f7c8c
brandonruizmora <brandon_ruizmor@outlook.es>
2021-11-09 22:23:30 -0600

    added sass styles

--- src/routes/AuthRouter.js
+++ src/routes/AuthRouter.js
@@ -5,12 +5,14 @@ import { RegisterScreen } from '../components/auth/RegisterScreen';
 
 export const AuthRouter = () => {
     return (
-        <>
+        <div className="auth__main">
+            <div className="auth__box-container">
             <Routes>
                 <Route path="login" element={<LoginScreen />} />
                 <Route path="register" element={<RegisterScreen />} />
                 <Route path="*" element={<Navigate replace to="login" />} />
             </Routes>
-        </>
+            </div>
+        </div>
     )
 }
