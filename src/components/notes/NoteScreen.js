c1db36762b95b456d26b3d05cbad84f1a5c0707d
brandonruizmora <brandon_ruizmor@outlook.es>
2021-11-25 19:06:59 -0600

    Notes style and inputs

--- src/components/notes/NoteScreen.js
+++ src/components/notes/NoteScreen.js
@@ -5,6 +5,13 @@ export const NoteScreen = () => {
     return (
         <div className="notes__main-content">
             <NotesAppBar />
+            <div className="notes__content">
+                <input type="text" placeholder="Some awesome title" className="notes__title-input" autoComplete="off" />
+                <textarea placeholder="What happend today" className="notes__textarea"></textarea>
+                <div className="notes__image">
+                    <img src="https://cdn.mos.cms.futurecdn.net/FRLZyoFp8gr9rApDDsx8mg.jpg" alt="imagen" />
+                </div>
+            </div>
         </div>
     )
 }
